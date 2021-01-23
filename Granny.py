0b0be70860d190d9fa81019be26f2361f8b056cb
Mohamed Hamza <mohamza@mohameds-imac.home>
2021-01-23 19:35:02 +0000

    granny.py documented

--- Granny.py
+++ Granny.py
@@ -19,13 +19,13 @@ class Granny(Lifeform):
 
     def walk(self):
 
-        super().move(2, self.vel)
+        self.move(2, self.vel)               # Super function call to move in the desired direction.
 
-        if self.x > self.window_width - super().icon.get_width():
-            self.direction = "left"
-            self.vel = self.vel * (-1)
-            super().icon = self.img[self.direction]
-        elif self.x < super().icon.get_width():
+        if self.x > self.window_width - super().icon.get_width():   # Hits the left wall
+            self.direction = "left"                     # to query our dictionary of granny assets later.
+            self.vel = self.vel * (-1)                  # to switch the direction she walks
+            super().icon = self.img[self.direction]     # update the granny icon, to reflect change of direction.
+        elif self.x < super().icon.get_width():         # Hits the right wall
             self.direction = "right"
             self.vel = self.vel * (-1)
             super().icon = self.img[self.direction]
